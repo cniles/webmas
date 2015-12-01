e59ac6f768383aaf1a4b633d3b0f089b2e8e9c1e
Craig Niles <niles.c@gmail.com>
2015-12-01 03:17:49 +0000

    Add a bunch of comments and clean up the code

--- xmas.py
+++ xmas.py
@@ -2,20 +2,20 @@ from flask import Flask,redirect
 import wiringpi2
 import json
 
-counter = 0
-
+# Creates our backend flask application. Set the static url path to
+# root, so that index.html can fall at /index.html
 app = Flask(__name__, static_url_path='')
-@app.route('/')
-def index():
-    return redirect("/index.html", code=302)
 
 # valid light numbers
 valid_lights = [0, 1, 2, 3]
 
 # Maps light number to gpio pin (wiringpi2 mapping)
-gpio_map = { 0: 7, 1: 0, 2: 1, 3: 4 }
+gpio_map = { 0: 7,
+             1: 0,
+             2: 1,
+             3: 4 }
 
-# maps light # to description
+# maps light nmber to a description
 descriptions = {
     0: "Stairs/balcony",
     1: "Plants/Shrubs",
@@ -29,12 +29,16 @@ status_map = {
     1: 'On'
 }
 
+# Gets a light current status
 def getLightStatus(num):
     return wiringpi2.digitalRead(gpio_map[num])
 
+# Sets a light on or off
 def setLightStatus(num, val):
     wiringpi2.digitalWrite(gpio_map[num], val)
 
+# Creates a new dict that describes the current state of a light,
+# which can then be encoded into JSON
 def makeLight(num):
     status = getLightStatus(num);
     return {
@@ -44,7 +48,13 @@ def makeLight(num):
         "name": "Light " + str(num+1),
         "num": num }
 
-@app.route('/light/<int:num>', methods=['POST','GET'])
+# Redirect root to index.html
+@app.route('/')
+def index():
+    return redirect("/index.html", code=302)
+
+# Toggles a light's status.  The response body is ignored
+@app.route('/light/<int:num>', methods=['POST'])
 def toggle_light(num):
     if light in valid_lights:
         status = getLightStatus(num)
@@ -53,15 +63,21 @@ def toggle_light(num):
         return json.dumps(makeLight(num))
     return json.dumps({"error":"Not a valid pin"})
 
+# Gets the status of all the lights
 @app.route('/lights', methods=['GET'])
 def get_lights():
     return json.dumps([ makeLight(num) for num in valid_lights ])
 
+# Fire it up
 if __name__ == '__main__':
+
+    # initialize wiringpi2 services
     wiringpi2.wiringPiSetup()
 
     # set gpio to output (if not already)
     for light in valid_lights:
         wiringpi2.pinMode(gpio_map[light], 1)
 
+    # start the web services
+    # host="0.0.0.0" allows external access
     app.run(port=80, host="0.0.0.0")
